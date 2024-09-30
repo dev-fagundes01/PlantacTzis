282e51f12ec013b5757d4787b975c5900654f6cc
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-09-30 20:38:22 -0300

    feat: Added image upload functionality to the RegisterProduct page.

--- src/pages/RegisterProduct.jsx
+++ src/pages/RegisterProduct.jsx
@@ -1,7 +1,8 @@
 import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { app } from '../../config/firebaseConfig'
+import { app, storage } from '../../config/firebaseConfig'
 import Background from "../components/Background";
 import Title from "../components/Title";
 
@@ -9,6 +10,8 @@ function RegisterProduct() {
   const [name, setName] = useState('')
   const [price, setPrice] = useState('')
   const [category, setCategory] = useState('')
+  const [imgURL, setImgURL] = useState('')
+  const [progress, setProgress] = useState(0)
 
   const navigate = useNavigate()
 
@@ -25,33 +28,59 @@ function RegisterProduct() {
     registeredProduct = product
   }
 
+  const handleUpload = (event) => {
+    event.preventDefault()
+
+    const file = event.target[3]?.files[0]
+    if (!file) return
+
+    const storageRef = ref(storage, `images/${file.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, file)
+
+    uploadTask.on(
+      "state_changed",
+      snapshot => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        setProgress(progress)
+      },
+      error => {
+        alert(error)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          setImgURL(url)
+        })
+      }
+    )
+  }
+
   return (
     <div className='h-screen flex flex-col items-center bg-primaryBackground'>
-      <div className='p-6 flex justify-center items-center'>
-        <Background />
-      </div>
-      <form>
-        <Title>Cadastre suas Plantas</Title>
+      <Background />
+      <Title>Cadastre suas Plantas</Title>
+      <form className='h-42. flex flex-col relative' onSubmit={handleUpload}>
 
-        <div className='my-5 flex flex-col'>
-          <label className='text-white mr-1' htmlFor="name">Nome:</label>
-          <input className='text-black rounded-lg p-1' placeholder="Tulipas" type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
-        </div>
-        <div className='my-5 flex flex-col'>
-          <label className='text-white mr-2' htmlFor="price">Preço:</label>
-          <input className='text-black rounded-lg p-1' placeholder="15" type="price" name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
-        </div>
-        <div className='my-5 flex flex-col'>
-          <label className='text-white mr-4' htmlFor="Category">Categoria:</label>
-          <input className='text-black rounded-lg p-1' placeholder="Plantas com Flores" type="text" name="Category" value={category} onChange={(e) => setCategory(e.target.value)} />
-        </div>
-        <button className='btn-secondary w-48 mx-20' type="submit" onClick={createProduct}>Cadastrar</button>
+        <label className='text-white mr-1' htmlFor="name">Nome:</label>
+        <input className='text-black rounded-lg p-1' placeholder="Tulipas" type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+
+        <label className='text-white mr-2' htmlFor="price">Preço:</label>
+        <input className='text-black rounded-lg p-1' placeholder="15" type="price" name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+
+        <label className='text-white mr-4' htmlFor="Category">Categoria:</label>
+        <input className='text-black rounded-lg p-1' placeholder="Plantas com Flores" type="text" name="Category" value={category} onChange={(e) => setCategory(e.target.value)} />
+
+        <label className='text-white mr-4' htmlFor="Image">Imagem:</label>
+        <input className='text-black rounded-lg p-1' type="file" name='Image' accept='image/' />
+        <button className='btn-primary w-48 mx-20 mt-6' type="submit">Cadastrar</button>
+
+        {!imgURL ? '' : <progress className='progress-custom w-full absolute bottom-14' value={progress} max="100" />}
+
       </form>
       {registeredProduct.name ?
         <p>{registeredProduct.name ? `${registeredProduct.name} registrado com sucesso!` : 'Produto não foi registrado'}</p>
         : ''
       }
-      <button className='btn-primary' type="button" onClick={() => navigate("/loja")}>Ir para a loja</button>
+      <button className='btn-secondary' type="button" onClick={() => navigate("/loja")}>Ir para a loja</button>
     </div>
   );
 }
