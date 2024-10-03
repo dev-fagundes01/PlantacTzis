ae21db6774efb3c7c2140528881ad2dc8bf10b6b
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-10-03 10:50:56 -0300

    refactor: Refactored PlantShop component: removed menu, added Header component, updated styles, and modified layout and icons

--- src/pages/screens/PlantShop.jsx
+++ src/pages/screens/PlantShop.jsx
@@ -1,11 +1,12 @@
-import { collection, getFirestore, onSnapshot } from 'firebase/firestore'
-import { Menu, X } from 'lucide-react';
+import { collection, onSnapshot } from 'firebase/firestore'
 import { useEffect, useState } from 'react';
 import { db } from '../../../config/firebaseConfig'
 import Slider from '../../components/Slider';
 
+import { CreditCard, Flower, Leaf, Truck } from 'lucide-react';
+import Header from '../../components/Header';
+
 export default function PlantShop() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [products, setProducts] = useState([]);
 
   const productCollectionRef = collection(db, 'products')
@@ -21,36 +22,17 @@ export default function PlantShop() {
   }, [productCollectionRef]);
 
   return (
-    <div className="min-h-screen bg-green-50">
-      <nav className="bg-green-600 p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-white text-2xl font-bold">DF-Plantas</h1>
-          <div className="flex items-center">
-            <button
-              type="button"
-              className="text-white lg:hidden"
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-            >
-              {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
-            </button>
-            <div className={`lg:flex ${isMenuOpen ? 'block' : 'hidden'} mt-4 lg:mt-0`}>
-              <a href="#l" className="block lg:inline-block text-white hover:text-green-200 mt-2 lg:mt-0 lg:ml-4">Início</a>
-              <a href="#l" className="block lg:inline-block text-white hover:text-green-200 mt-2 lg:mt-0 lg:ml-4">Produtos</a>
-              <a href="#l" className="block lg:inline-block text-white hover:text-green-200 mt-2 lg:mt-0 lg:ml-4">Sobre</a>
-              <a href="#l" className="block lg:inline-block text-white hover:text-green-200 mt-2 lg:mt-0 lg:ml-4">Contato</a>
-            </div>
-          </div>
-        </div>
-      </nav>
+    <div className="min-h-screen bg-primaryBackground">
+      <Header />
 
       <main className="container mx-auto p-8">
         <Slider title={'Nossas Plantas'} info={products} />
       </main>
-      <section className='w-max mx-auto flex gap-4'>
+      <section className='h-32 w-max mx-auto flex gap-8'>
         <div>
           <p className='p w-56'>
-            <h3 className='h2 text-lg'>
+            <h3 className='h3 text-lg'>
+              <Flower />
               Plantas Premium
             </h3>
             Mudas de Plantas de alta qualidade, produção artesanal.
@@ -58,7 +40,8 @@ export default function PlantShop() {
         </div>
         <div>
           <p className='p w-56'>
-            <h3 className='h2 text-lg'>
+            <h3 className='h3 text-lg'>
+              <Leaf />
               Espécies Exóticas
             </h3>
             Plantas Raras e Exóticas que você só encontra em nosso viveiro.
@@ -66,7 +49,8 @@ export default function PlantShop() {
         </div>
         <div>
           <p className='p w-56'>
-            <h3 className='h2 text-lg'>
+            <h3 className='h3 text-lg'>
+              <CreditCard />
               Parcele 12x Sem Juros
             </h3>
             Parcele suas compras em até 12x sem juros no cartão de crédito.
@@ -74,7 +58,8 @@ export default function PlantShop() {
         </div>
         <div>
           <p className='p w-56'>
-            <h3 className='h2 text-lg'>
+            <h3 className='h3 text-lg'>
+              <Truck />
               Entrega Grátis
             </h3>
             Entrega grátis em todo o Brasil para compras acima de R$250.
@@ -82,10 +67,12 @@ export default function PlantShop() {
         </div>
       </section>
 
-      <footer className="w-full py-8 bg-green-800 text-white">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7894.496372492319!2d-35.24118454950209!3d-8.377230898066777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1727961728152!5m2!1spt-BR!2sbr" width="1000" height="450" style={{border: "0", marginInline: "auto", padding: "10px"}} allowfullscreen="" loading="lazy" title="loja df-plantas" referrerpolicy="no-referrer-when-downgrade">DF-Plantas</iframe>
+
+      <footer className="w-full py-8 bg-thirdBackground text-secondaryForeground">
         <div className="container mx-auto flex justify-between text-center">
           <p>Todos os Direitos Reservados &copy; {new Date().getFullYear()} DF-Plantas</p>
-          <a href="https://www.linkedin.com/in/diego-fagundes-da-silva/">
+          <a href="https://diego-fagundes-links.netlify.app/?fbclid=PAZXh0bgNhZW0CMTEAAaYKeVzi1XXM9tgpIFd_ZjC8z1dDtiTkAPR0Ttj9o6LtylYial0w_Ur86Do_aem_MK27d1KMJgfb4miHo5RofQ" target="_blank" rel="noreferrer">
             <p>
               Desenvolvido por Diego Fagundes
             </p>
