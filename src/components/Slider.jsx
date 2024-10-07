c0b52a22945a549e12e8d76242c066f0fa82692a
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-10-06 22:55:16 -0300

    feat: update Slider component with conditional title rendering and swiper class changes
    - Added conditional rendering for the title in the Slider component.
    - Changed swiper class for improved styling and functionality.
    - Modified SwiperSlide key and class for better structure and performance.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,16 +6,17 @@ import Card from './Card';
 export default function Slider({ info, title }) {
   return (
     <>
-      <h2 className='h2'>{title}</h2>
+      {info.length > 0 &&
+        <h2 className='h2-c'>{title}</h2>
+      }
       <Swiper
         spaceBetween={50}
         slidesPerView={3}
-        className='swiper py-4'
+        className='swiper p-2 md:py-4'
         onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
       >
         {info.map((item) => (
-          <SwiperSlide className='!w-52 h-80' key={item.id}>
+          <SwiperSlide key={item.id || item.image} className='md:!w-52 md:h-80'>
             <Card item={item} />
           </SwiperSlide>
         ))}
