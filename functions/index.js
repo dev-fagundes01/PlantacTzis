dc15f1b133a39049ee20887839ea437e06bbd83e
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-10-15 12:47:54 -0300

    chore(config): Updated .gitignore, firebase.json, package-lock.json, and package.json files with new dependencies and configurations

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+/**
+Import function triggers from their respective submodules:
+See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
