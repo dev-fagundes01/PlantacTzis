e7373b2b5753826842dbedd236cd88791f65ad45
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-10-21 23:29:07 -0300

    feat: enhance product management and layout
    - Added useEffect to set initial products state
    - Changed product filtering buttons to horizontal layout
    - Updated Card component to receive admin prop

--- src/pages/ManageProducts.jsx
+++ src/pages/ManageProducts.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Background from "../components/Background";
 import Card from "../components/CardProduto";
 import Title from '../components/Title'
@@ -6,25 +6,29 @@ import { useProduct } from '../context/ProductContext'
 
 export default function ManageProducts() {
   const { plants, vases, other } = useProduct()
-  const [products, setProducts] = useState(plants)
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    if (plants.length > 0) {
+      setProducts(plants)
+    }
+  }, [plants])
 
   return (
     <div className='h-screen p-2 bg-secondaryBackground flex flex-col overflow-y-auto md:items-center relative'>
       <Background />
       <Title>Seus Produtos</Title>
 
-      <div className="flex">
-        <div className="w-[70vw] mt-2 flex flex-wrap gap-2">
-          {products?.map((plant) => (
-            <Card key={plant.id} produto={plant} />
-          ))}
-        </div>
+      <div className="flex gap-3">
+        <button className="px-2 rounded-sm bg-primaryBackground hover:opacity-80 active:opacity-50" type="button" onClick={() => setProducts(plants)}>Plantas</button>
+        <button className="px-2 rounded-sm bg-primaryBackground hover:opacity-80 active:opacity-50" type="button" onClick={() => setProducts(vases)}>Vasos</button>
+        <button className="px-2 rounded-sm bg-primaryBackground hover:opacity-80 active:opacity-50" type="button" onClick={() => setProducts(other)}>Outros</button>
+      </div>
 
-        <div className="mt-2 flex flex-col">
-          <button className="rounded-sm bg-primaryBackground hover:opacity-80 active:opacity-50" type="button" onClick={() => setProducts(plants)}>Plantas</button>
-          <button className="rounded-sm bg-primaryBackground hover:opacity-80 active:opacity-50" type="button" onClick={() => setProducts(vases)}>Vasos</button>
-          <button className="rounded-sm bg-primaryBackground hover:opacity-80 active:opacity-50" type="button" onClick={() => setProducts(other)}>Outros</button>
-        </div>
+      <div className="max-w-[80vw] mt-2 flex flex-wrap justify-center gap-2">
+        {products.map((product) => (
+          <Card key={product.id} produto={product} admin={true} />
+        ))}
       </div>
     </div>
   )
